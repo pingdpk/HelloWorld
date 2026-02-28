185f203d28e433fa677af37dd83dc05b2779c1e3
Deepak Edakkott <v-dedakkott@egencia.com>
2026-02-28 14:00:00 +0400

    app with Main

---
+++ src/main/java/com/evra/ocppcharging/OcppChargingApplication.java
@@ -0,0 +1,12 @@
+package com.evra.ocppcharging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcppChargingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcppChargingApplication.class, args);
+    }
+}
